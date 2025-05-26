f040409a276aa7aa4dde52892fce88fcb0c17987
cyfewlp <2213662140@qq.com>
2025-05-27 07:00:04 +0800

    SimpleIME: skip load failed IME

--- common/WCharUtils.h
+++ common/WCharUtils.h
@@ -5,42 +5,56 @@
 
 #include "common/config.h"
 
-#include <windows.h>
 #include <string>
+#include <windows.h>
 
 namespace LIBC_NAMESPACE_DECL
 {
-    class WCharUtils
+class WCharUtils
+{
+public:
+    static auto ToString(const std::wstring &wstr, const UINT codePage = CP_UTF8) -> std::string
     {
-    public:
-        static auto ToString(const std::wstring &wstr, const UINT codePage = CP_UTF8) -> std::string
-        {
-            int const   size_needed = WideCharToMultiByte(codePage, 0, wstr.data(), static_cast<int>(wstr.size()),
-                                                          nullptr, 0, nullptr, nullptr);
-            std::string strTo(size_needed, 0);
-            WideCharToMultiByte(codePage, 0, wstr.data(), static_cast<int>(wstr.size()), strTo.data(), size_needed,
-                                nullptr, nullptr);
-            return strTo;
-        }
+        int const size_needed =
+            WideCharToMultiByte(codePage, 0, wstr.data(), static_cast<int>(wstr.size()), nullptr, 0, nullptr, nullptr);
+        std::string strTo(size_needed, 0);
+        WideCharToMultiByte(
+            codePage, 0, wstr.data(), static_cast<int>(wstr.size()), strTo.data(), size_needed, nullptr, nullptr
+        );
+        return strTo;
+    }
 
-        static auto ToString(const wchar_t *pwsz, const int charSize, const UINT codePage = CP_UTF8) -> std::string
-        {
-            int const   size_needed = WideCharToMultiByte(codePage, 0, pwsz, charSize, nullptr, 0, nullptr, nullptr);
-            std::string strTo(size_needed, 0);
-            WideCharToMultiByte(codePage, 0, pwsz, charSize, strTo.data(), size_needed, nullptr, nullptr);
-            return strTo;
-        }
+    static auto ToString(const wchar_t *pwsz, const int charSize, const UINT codePage = CP_UTF8) -> std::string
+    {
+        int const   size_needed = WideCharToMultiByte(codePage, 0, pwsz, charSize, nullptr, 0, nullptr, nullptr);
+        std::string strTo(size_needed, 0);
+        WideCharToMultiByte(codePage, 0, pwsz, charSize, strTo.data(), size_needed, nullptr, nullptr);
+        return strTo;
+    }
 
-        static constexpr auto RequiredByteLength(const LPCWCH lpcWch, const UINT codePage = CP_UTF8) -> int
-        {
-            return WideCharToMultiByte(codePage, 0, lpcWch, -1, nullptr, 0, nullptr, nullptr);
-        }
+    static constexpr auto RequiredByteLength(const LPCWCH lpcWch, const UINT codePage = CP_UTF8) -> int
+    {
+        return WideCharToMultiByte(codePage, 0, lpcWch, -1, nullptr, 0, nullptr, nullptr);
+    }
 
-        static constexpr void ToString(const LPCWCH lpcwch, __out LPSTR lpStr, int length, UINT codePage = CP_UTF8)
+    static constexpr void ToString(const LPCWCH lpcwch, __out LPSTR lpStr, int length, UINT codePage = CP_UTF8)
+    {
+        WideCharToMultiByte(codePage, 0, lpcwch, -1, lpStr, length, nullptr, nullptr);
+    }
+
+    static auto ToString(
+        const wchar_t *pwsz, const int charSize, __out std::string &outStr, const UINT codePage = CP_UTF8
+    ) -> bool
+    {
+        int const size_needed = WideCharToMultiByte(codePage, 0, pwsz, charSize, nullptr, 0, nullptr, nullptr);
+        if (size_needed > 0)
         {
-            WideCharToMultiByte(codePage, 0, lpcwch, -1, lpStr, length, nullptr, nullptr);
+            outStr.resize(size_needed);
+            return WideCharToMultiByte(codePage, 0, pwsz, charSize, outStr.data(), size_needed, nullptr, nullptr) != 0;
         }
-    };
+        return false;
+    }
+};
 } // namespace LIBC_NAMESPACE_DECL
 
 #endif
