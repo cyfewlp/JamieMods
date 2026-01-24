bb4eb612745005ef3a9cc6d2533752c86cc7f4a5
cyfewlp <2213662140@qq.com>
2026-01-24 23:12:14 +0800

    dev: remove Tsf/globalhooks test

--- dev/BasicTest.cpp
+++ dev/BasicTest.cpp
@@ -1,49 +1,35 @@
-﻿#include "common/toml++/toml.hpp"
+﻿#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <uchar.h>
 
-#include <array>
-#include <iostream>
-#include <ostream>
-
-int main()
+int main(void)
 {
-    struct ImGuiScope
+    setlocale(LC_ALL, "en_US.utf8");
+    const char16_t in[] = u"zß水🍌"; // or "z\u00df\u6c34\U0001F34C"
+    const size_t in_sz = sizeof in / sizeof *in;
+
+    printf("Processing %zu UTF-16 code units: [", in_sz);
+    for (size_t n = 0; n < in_sz; ++n)
+        printf("%s%04X", n ? " " : "", in[n]);
+    puts("]");
+
+    char* out = malloc(MB_CUR_MAX * in_sz);
+    char* p = out;
+    mbstate_t state = {0};
+
+    for (size_t n = 0; n < in_sz; ++n)
     {
-        ImGuiScope()
-        {
-            std::cout << __func__ << std::endl;
-        }
-
-        virtual ~ImGuiScope()
-        {
-            std::cout << __func__ << std::endl;
-        }
-    };
-
-    struct ImGuiColorScope : ImGuiScope
-    {
-        ImGuiColorScope(uint32_t colorIndex)
-        {
-            std::cout << "ImGuiColorScope() " << colorIndex << std::endl;
-        }
-
-        ~ImGuiColorScope() override
-        {
-            std::cout << "~ImGuiColorScope" << std::endl;
-        }
-    };
-
-    std::array<ImGuiScope, 3> scopes = {};
-
-    scopes[0] = ImGuiScope();
-    scopes[1] = ImGuiScope();
-    scopes[2] = ImGuiScope();
-
-    {
-        std::cout << "enter block\n";
-        scopes[1] = ImGuiColorScope(1);
-        scopes[2] = ImGuiColorScope(3);
-        std::cout << "exit block\n";
+        size_t rc = c16rtomb(p, in[n], &state);
+        if (rc == (size_t)-1)
+            break;
+        p += rc;
     }
 
-    return 0;
+    size_t out_sz = p - out;
+    printf("into %zu UTF-8 code units: [", out_sz);
+    for (size_t x = 0; x < out_sz; ++x)
+        printf("%s%02X", x ? " " : "", +(unsigned char)out[x]);
+    puts("]");
+    free(out);
 }
