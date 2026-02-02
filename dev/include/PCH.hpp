9d46ca6f4496203359a85d5f6b8c354501efc03c
cyfewlp <221366214@qq.com>
2026-02-02 15:34:09 +0800

    Move dev tools to untracked private directory

--- dev/include/PCH.hpp
+++
@@ -1,5 +0,0 @@
-
-#include "spdlog/spdlog.h"
-
-#define LOG(a_type, ...)                                                                                               \
-    spdlog::log(spdlog::source_loc(__FILE__, __LINE__, __FUNCTION__), spdlog::level::a_type, __VA_ARGS__)
