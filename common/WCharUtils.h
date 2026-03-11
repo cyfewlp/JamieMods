f5a5a4170e097e19fb22e2ca48aa2637d1afff77
cyfewlp <221366214@qq.com>
2026-03-11 23:34:37 +0800

    fix(SimpleIME): fix GFxCharEvent memory leak in ImeMenu
    GFxCharEvent objects allocated via SendUiString were never freed because
    BSUIScaleformData's destructor does not delete scaleformEvent. ImeMenu
    now collects kImeCharEvent pointers during ProcessMessage and frees them
    in PostDisplay using RE::GMemory::Free. Safe because PostDisplay always
    runs after ProcessMessage on the same render thread within a given frame.
    
    Also fix incorrectly retained LANG_PROFILE_ACTIVATED state when using
    English input: remove the automatic insertion of DEFAULT_LANG_PROFILE
    into m_langProfiles in RefreshProfiles, and gate OnCharEvent handling on
    !ImeDisabled() in addition to LANG_PROFILE_ACTIVATED.
    refactor(SimpleIME): replace Utils::SendStringToGame with Skyrim::SendUiString
    
    Remove the Utils class and move the char-sending logic to
    Ime::Skyrim::SendUiString, implemented in a dedicated source file.
    Update all call sites.
    
    refactor(common): convert WCharUtils class to namespace
    
    Replace the WCharUtils static-method class with a namespace of free
    functions. Remove unused overloads; std::wstring_view is now the only
    accepted parameter type. Update all call sites.

--- common/WCharUtils.h
+++ common/WCharUtils.h
@@ -6,58 +6,23 @@
 #include <string>
 #include <windows.h>
 
-class WCharUtils
+namespace WCharUtils
 {
-public:
-    static auto ToString(const std::wstring_view &wstrv, const UINT codePage = CP_UTF8) -> std::string
-    {
-        const int size_needed = WideCharToMultiByte(codePage, 0, wstrv.data(), static_cast<int>(wstrv.size()), nullptr, 0, nullptr, nullptr);
-        if (size_needed > 0)
-        {
-            std::string strTo;
-            strTo.resize(static_cast<size_t>(size_needed));
-            int written = WideCharToMultiByte(codePage, 0, wstrv.data(), static_cast<int>(wstrv.size()), strTo.data(), size_needed, nullptr, nullptr);
-            if (written > 0)
-            {
-                return strTo;
-            }
-        }
-        return {};
-    }
-
-    static auto ToString(const wchar_t *pwsz, const int charSize, const UINT codePage = CP_UTF8) -> std::string
+inline auto ToString(const std::wstring_view &wstrv, const UINT codePage = CP_UTF8) -> std::string
+{
+    const int size_needed = WideCharToMultiByte(codePage, 0, wstrv.data(), static_cast<int>(wstrv.size()), nullptr, 0, nullptr, nullptr);
+    if (size_needed > 0)
     {
         std::string strTo;
-        const int   size_needed = WideCharToMultiByte(codePage, 0, pwsz, charSize, nullptr, 0, nullptr, nullptr);
-        if (size_needed > 0)
+        strTo.resize(static_cast<size_t>(size_needed));
+        int written = WideCharToMultiByte(codePage, 0, wstrv.data(), static_cast<int>(wstrv.size()), strTo.data(), size_needed, nullptr, nullptr);
+        if (written > 0)
         {
-            strTo.resize(static_cast<size_t>(size_needed));
-            WideCharToMultiByte(codePage, 0, pwsz, charSize, strTo.data(), size_needed, nullptr, nullptr);
+            return strTo;
         }
-        return strTo;
     }
+    return {};
+}
 
-    static constexpr auto RequiredByteLength(const LPCWCH lpcWch, const UINT codePage = CP_UTF8) -> int
-    {
-        return WideCharToMultiByte(codePage, 0, lpcWch, -1, nullptr, 0, nullptr, nullptr);
-    }
-
-    static constexpr void ToString(const LPCWCH lpcwch, __out LPSTR lpStr, const int length, UINT codePage = CP_UTF8)
-    {
-        WideCharToMultiByte(codePage, 0, lpcwch, -1, lpStr, length, nullptr, nullptr);
-    }
-
-    static auto ToString(const wchar_t *pwsz, const int charSize, __out std::string &outStr, const UINT codePage = CP_UTF8) -> bool
-    {
-        const int size_needed = WideCharToMultiByte(codePage, 0, pwsz, charSize, nullptr, 0, nullptr, nullptr);
-        if (size_needed > 0)
-        {
-            const size_t oldSize = outStr.size();
-            outStr.resize(oldSize + static_cast<size_t>(size_needed));
-            return WideCharToMultiByte(codePage, 0, pwsz, charSize, outStr.data() + oldSize, size_needed, nullptr, nullptr) != 0;
-        }
-        return false;
-    }
-};
-
+} // namespace WCharUtils
 #endif
